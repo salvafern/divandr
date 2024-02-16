07eada0593f944c0047c67f223f1b9f8323a9197
salvafern <salvador.fernandez@vliz.be>
2024-02-16 12:34:45 +0100

    feat: first attempt interpolation fun diva_run()

--- R/main.R
+++ R/main.R
@@ -6,7 +6,7 @@
 #' if they are missing.
 #'
 #' @param pkg_check logical, check for DIVAnd.jl package and install if necessary
-#' @param ... Parameters are passed down to JuliaCall::julia_setup
+#' @param ... Parameters are passed down to JuliaCall::julia_setup()
 #'
 #' @examples
 #'
@@ -21,9 +21,76 @@
 diva_setup <- function (pkg_check=TRUE,...){
   julia <- JuliaCall::julia_setup(installJulia=TRUE,...)
   if(pkg_check){
-    # JuliaCall::julia_install_package_if_needed("Statistics")
-    # JuliaCall::julia_install_package_if_needed("NCDatasets")
+    JuliaCall::julia_install_package_if_needed("NCDatasets")
     JuliaCall::julia_install_package_if_needed("DIVAnd")
   }
   JuliaCall::julia_library("DIVAnd")
 }
+
+#' DIVAnd Interpolation
+#'
+#' @param x numerical. Vector (1D), Matrix (2D) or Array (ND) with data
+#' @param f
+#' @param n numerical. Set the interpolation grid: increase the value to have
+#' a finer resolution
+#' @param len
+#' @param epsilon2
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Start up DIVAnd and Julia
+#' diva_setup()
+#'
+#' # Create 1D data
+#' x <- c(0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.1)
+#' f <- sin(x*6)
+#'
+#' # Set resolution via number of points
+#' n <- 30
+#'
+#' # Analysis parameters
+#' # Correlation length
+#' len <- 0.1
+#'
+#' # Obs. error variance normalized by the background error variance
+#' epsilon2 <- 0.1
+#'
+#' # Run
+#' output <- diva_run(x, f, n, len, epsilon2)
+#' output
+#' }
+diva_run <- function(x, f, n, len, epsilon2){
+
+  # Set the interpolation grid: increase the value of n to have a finer resolution
+  xi <- seq(0, 1, length.out=n)
+
+  # True field (normally unknown)
+  fref <- sin(xi * 6)
+
+  # Mask: all points are valid points
+  mask <- !logical(length(xi))
+
+  # Metrics: pm is the inverse of the resolution along the 1st dimension
+  pm <- rep(1, length(xi)) / (xi[2]-xi[1])
+
+  # From R variable to Julia variable
+  # (maybe possible to do it differently)
+  JuliaCall::julia_assign("pm", pm)
+  JuliaCall::julia_assign("xi", xi)
+  JuliaCall::julia_assign("x", x)
+  JuliaCall::julia_assign("f", f)
+  JuliaCall::julia_assign("len", len)
+  JuliaCall::julia_assign("mask", mask)
+  JuliaCall::julia_assign("epsilon2", epsilon2)
+
+  # DIVAnd execution
+  JuliaCall::julia_command("fi, s = DIVAndrun(mask, (pm,) ,(xi,), (x,), f, len, epsilon2; alphabc=0);")
+
+  # From Julia variable to R variable
+  fi = JuliaCall::julia_eval("fi")
+
+  fi
+}
+
